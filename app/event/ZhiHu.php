3c5c4988e6d3892bf6d56bcf5989ada92cf49359
joker9657 <zqf2102398@gmail.com>
2024-06-07 14:38:11 +0800

    feat: 更新zhihu数据获取

--- app/event/ZhiHu.php
+++ app/event/ZhiHu.php
@@ -10,7 +10,7 @@ use support\Redis;
 class ZhiHu
 {
     //知乎的热榜接口可以直接请求
-    public const url = 'https://www.zhihu.com/api/v4/creators/rank/hot?domain=0&period=hour&limit=30';
+    public const url = 'https://www.zhihu.com/api/v4/feed/topstory/hot-lists/total?limit=30';
 
     public const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36';
 
@@ -33,13 +33,14 @@ class ZhiHu
 
             $insertData = [];
             foreach ($res['data'] as $item) {
-                $title = $item['question']['title'];
-                $url = $item['question']['url'];
-                $subtitle = $item['reaction']['pv'];
+                $title = $item['target']['title'];
+                $url = "https://www.zhihu.com/question/{$item['target']['id']}";
+                $hot = explode(' ', $item['detail_text']);
+                $subtitle = $hot[0] ?? 0;
                 $insertData[] = [
                     'title'       => $title,
                     'url'         => $url,
-                    'subtitle'    => $subtitle,
+                    'subtitle'    => $subtitle * 10000,
                     'date'        => Carbon::now()->toDateString(),
                     'create_time' => Carbon::now()->toDateTimeString()
                 ];
